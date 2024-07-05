4bdf06e29986694375993469bec74ec7e029eca8
Gabriela Oliveira <113140018+GabrielaOlv7@users.noreply.github.com>
2024-07-05 19:32:48 +0000

    Endpoint Read By Id [GET]/materia/:id criado

--- index.js
+++ index.js
@@ -9,5 +9,12 @@ const lista = ['c++','Python','POO Java']
 app.get('/materia', function (req,res){
   res.send(lista)
 })
-
+//endpoint read by id [GET]/materia/:id
+app.get('/materia/:id', function (req,res){
+  //acessamos o parametro de rota id
+  const id = req.params.id
+  //acessa o item da lista usando id -1
+  const item = lista [id-1]
+  res.send(item)
+})
 app.listen(3000)
