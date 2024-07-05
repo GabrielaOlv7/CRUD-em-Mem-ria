b4c91064c80b85757c2ec958c702599984d6b89b
Gabriela Oliveira <113140018+GabrielaOlv7@users.noreply.github.com>
2024-07-05 19:23:25 +0000

    Endpoint read all [GET]/materia implementado

--- index.js
+++ index.js
@@ -4,5 +4,10 @@ const app = express()
 app.get('/', function (req, res) {
   res.send('Hello World')
 })
+const lista = ['c++','Python','POO Java']
+//Read all [GET] /matérias
+app.get('/materia', function (req,res){
+  res.send(lista)
+})
 
 app.listen(3000)
